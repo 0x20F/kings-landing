a832f7bbb3ac9890d90107102bb606058f6a1642
Poly <alexhexan22@gmail.com>
2020-12-17 18:51:26 +0100

    Cleanup and prepare for new colors

--- src/index.js
+++ src/index.js
@@ -5,13 +5,4 @@ import App from './app/App'
 import 'styles/app'
 
 
-// Setup particles.js
-import pJS from './app/lib/particles';
-import options from './app/lib/particleOptions';
-
-pJS('particles-container', options);
-
-
-
-
 ReactDOM.render(<App/>, document.getElementById('root'))
